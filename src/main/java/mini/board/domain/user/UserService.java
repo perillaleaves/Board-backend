0e44c1d84069cf35f34974cebf724450bc320552
gyunny <xorbs15923@naver.com>
2022-11-26 14:41:23 +0900

    Update : validate method separate

--- src/main/java/mini/board/domain/user/UserService.java
+++ src/main/java/mini/board/domain/user/UserService.java
@@ -3,17 +3,11 @@ package mini.board.domain.user;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
-
-    @PersistenceContext
-    private EntityManager em;
 
     private final UserRepository userRepository;
 
@@ -23,22 +17,37 @@ public class UserService {
 
     @Transactional
     public User save(User user) {
+        validate(user);
+
         LocalDateTime date = LocalDateTime.now();
         user.setCreatedAt(date);
 
-        Optional<User> userByLoginId = userRepository.findByLoginId(user.getLoginId());
-        if (userByLoginId.isPresent() ||
-                user.getLoginId().length() < 8 ||
-                user.getPassword().length() < 8 ||
-                user.getName().length() < 0 ||
-                user.getPhoneNum().length() < 0 ||
-                user.getEmail().length() < 0) {
-            return null;
-        } else {
-            User saveUser = userRepository.save(user);
-            return saveUser;
+        User saveUser = userRepository.save(user);
+
+        return saveUser;
+    }
+
+    private void validate(User user) {
+        if (user.getLoginId().length() < 8) {
+            throw new Error("아이디를 8글자 이상 입력해주세요.");
+        }
+        if (user.getPassword().length() < 8) {
+            throw new Error("비밀번호를 8글자 이상 입력해주세요.");
+        }
+        if (user.getName().length() < 0) {
+            throw new Error("이름을 입력해주세요.");
+        }
+        if (user.getPhoneNum().length() < 0) {
+            throw new Error("연락처를 입력해주세요.");
+        }
+        if (user.getEmail().length() < 0) {
+            throw new Error("이메일을 입력해주세요.");
         }
 
+        Optional<User> userByLoginId = userRepository.findByLoginId(user.getLoginId());
+        if (userByLoginId.isPresent()) {
+            throw new Error("이미 존재하는 아이디입니다.");
+        }
     }
 
 
