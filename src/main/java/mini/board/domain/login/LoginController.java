4d5e534020a1e72937de79197120e1d9258a4626
gyunny <needle12s@gmail.com>
2022-11-29 16:32:48 +0900

    feat : content-type : form-data -> json

--- src/main/java/mini/board/domain/login/LoginController.java
+++ src/main/java/mini/board/domain/login/LoginController.java
@@ -1,18 +1,14 @@
 package mini.board.domain.login;
 
 import mini.board.domain.user.User;
-import mini.board.domain.user.UserDTO;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3001")
 @RestController
 public class LoginController {
 
@@ -22,10 +18,10 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    @PostMapping("/signin")
-    public Map<String, Object> login(@ModelAttribute UserDTO userDTO, HttpServletRequest request) {
+    @PostMapping("/login")
+    public Map<String, Object> login(@RequestBody User user, HttpServletRequest request) {
         Map<String, Object> map = new HashMap<>();
-        Optional<User> loginUser = loginService.signIn(new User(userDTO.getLoginId(), userDTO.getPassword()));
+        User loginUser = loginService.signIn(user);
 
         if (loginUser != null) {
             HttpSession session = request.getSession();
@@ -39,6 +35,17 @@ public class LoginController {
         return map;
     }
 
+    @PostMapping("/logout")
+    public Map<String, Object> logout(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+            map.put("result", "logout");
+            System.out.println("logout");
+        }
 
+        return map;
+    }
 
 }
